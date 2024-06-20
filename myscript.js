2dffe3364e565eef17698e9e7be9da5860f04be8
Elisa Chiappini <elisabichiappini@gmail.com>
2024-06-20 21:43:46 +0200

    toFix: correction tag

--- myscript.js
+++ myscript.js
@@ -37,21 +37,21 @@ class AppTodo {
     }
 };
 
+//istanza classe 
 const todoList = new AppTodo();
 todoList.addTodo(new Todo('ciao'));
 todoList.addTodo(new Todo('ciao'));
 todoList.addTodo(new Todo('ciao'));
 
-
-//istanza classe 
-const result = todoList.getAllTodos();
 //variabili
+const result = todoList.getAllTodos();
 const elementButtonShow = document.getElementById('show-todos');
 const elementButtonHidden = document.getElementById('hidden-todos');
 const elementUl = document.querySelector('.todos'); 
 
 elementButtonShow.addEventListener('click', showTodos);
 elementButtonHidden.addEventListener('click', hiddenTodos);
+
 //aggiungere un nuovoTodo
 const elementButtonAdd = document.getElementById('add-todo');
 elementButtonAdd.addEventListener('click', function(event) {
